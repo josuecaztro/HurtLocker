2fcc03cbb2fc02e874019c9027e9b797a83aeace
Josue <josue@Zipcoders-MacBook-Pro-75.local>
2024-08-15 09:24:11 -0400

    Finished to my best degree

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,41 +16,53 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception{
-        String output = (new Main()).readRawDataToString();
-        System.out.println(output);
+        Main main = new Main();
 
-        String patternString = output;
-        Pattern pattern = Pattern.compile(patternString);
-        Matcher matcher = pattern.matcher("\\w+");
+        //REGEX
+        String fullText = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
+        String patternString = "Name:(.*?)[;@*%!^]Price:(.*?)[;@*%!^]type:(.*?)[;@*%!^]expiration:(.*?)#";
+        Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(fullText);
 
-    }
+        //PRICES
+        Map<Double, Integer> appleMap = main.createApplesPriceMap(fullText);//0.25 and 0.23
+        Map<Double, Integer> cookieMap = main.createCookiePriceMap(fullText);//2.25
+        Map<Double, Integer> milkMap = main.createMilkPriceMap(fullText);//3.23 and 1.23
+        Map<Double, Integer> breadMap = main.createBreadPriceMap(fullText);//1.23
 
-    public String formatStringData (String x){
-        String result = x
-                .replaceAll(":", " : ")
-                .replaceAll("[;@^*%!]", "\n==========\n");
-       return result;
-    }
+        StringBuilder sb = new StringBuilder();
+        //APPLES
+        sb.append(String.format("\nName: Apples%-"+18+"s Seen: %d times","",main.findApplesSeen(fullText)));
+        sb.append(String.format("\n=============== %-"+11+"s ================", ""));
+        sb.append(String.format("\nPrice: 0.25%-"+19+"s Seen: %d times","",appleMap.get(0.25)));
+        sb.append(String.format("\n---------------- %-"+10+"s ----------------", ""));
+        sb.append(String.format("\nPrice: 0.23%-"+19+"s Seen: %d times","",appleMap.get(0.23)));
+        sb.append("\n\n");
+        //COOKIES
+        sb.append(String.format("\nName: Cookies%-"+17+"s Seen: %d times","",main.findCookiesSeen(fullText)));
+        sb.append(String.format("\n=============== %-"+11+"s ================", ""));
+        sb.append(String.format("\nPrice: 2.25%-"+19+"s Seen: %d times","",cookieMap.get(2.25)));
+        sb.append(String.format("\n---------------- %-"+10+"s ----------------", ""));
+        sb.append("\n\n");
+        //MILK
+        sb.append(String.format("\nName: Milk%-"+20+"s Seen: %d times","",main.findMilkSeen(fullText)));
+        sb.append(String.format("\n=============== %-"+11+"s ================", ""));
+        sb.append(String.format("\nPrice: 0.25%-"+19+"s Seen: %d times","",milkMap.get(3.23)));
+        sb.append(String.format("\n---------------- %-"+10+"s ----------------", ""));
+        sb.append(String.format("\nPrice: 0.23%-"+19+"s Seen: %d times","",milkMap.get(1.23)));
+        sb.append("\n\n");
+        //BREAD
+        sb.append(String.format("\nName: Bread%-"+19+"s Seen: %d times","",main.findBreadSeen(fullText)));
+        sb.append(String.format("\n=============== %-"+11+"s ================", ""));
+        sb.append(String.format("\nPrice: 0.25%-"+19+"s Seen: %d times","",breadMap.get(1.23)));
+        sb.append(String.format("\n---------------- %-"+10+"s ----------------", ""));
+        sb.append("\n");
+        //ERRORS
+        sb.append(String.format("\nErrors:%-"+23+"s Seen: %d times","",4));
+        System.out.println(sb);
 
-    public String separatePairs(String x) {
-        String result = x
-                .replaceAll(":", " : ")
-                .replaceAll("[;@^*%!]", "\n==========\n")
-                .replaceAll("##", "\n-----------\n\n\n");
-        String realResult = String.format("%S", result);
-        return realResult;
-    }
 
-    public static int countOccurrences(String text, String regex) {
-        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(text);
-        int count = 0;
 
-        while (matcher.find()) {
-            count++;
-        }
-
-        return count;
     }
 
     public int findBreadSeen(String text) {
@@ -98,7 +110,7 @@ public class Main {
     }
 
     public Map<Double, Integer> createBreadPriceMap(String text) {
-        String normalizedItem = "Apples".toLowerCase();
+        String normalizedItem = "Bread".toLowerCase();
         String regex = "\\bnaMe:" + normalizedItem + ";price:(\\d+\\.\\d+);";
         Map<Double, Integer> priceCounts = new HashMap<>();
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
@@ -122,7 +134,34 @@ public class Main {
             double price = Double.parseDouble(priceStr);
             priceCounts.put(price, priceCounts.getOrDefault(price, 0) + 1);
         }
-        System.out.println(priceCounts);
+        return priceCounts;
+    }
+
+    public Map<Double, Integer> createMilkPriceMap(String text) {
+        String normalizedItem = "Milk".toLowerCase();
+        String regex = "\\bnaMe:" + normalizedItem + ";price:(\\d+\\.\\d+);";
+        Map<Double, Integer> priceCounts = new HashMap<>();
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            String priceStr = matcher.group(1);
+            double price = Double.parseDouble(priceStr);
+            priceCounts.put(price, priceCounts.getOrDefault(price, 0) + 1);
+        }
+        return priceCounts;
+    }
+
+    public Map<Double, Integer> createCookiePriceMap(String text) {
+        String normalizedItem = "Cookies".toLowerCase();
+        String regex = "\\bnaMe:" + normalizedItem.replace("o", "[o0]") + ";price:(\\d+\\.\\d+);";
+        Map<Double, Integer> priceCounts = new HashMap<>();
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            String priceStr = matcher.group(1);
+            double price = Double.parseDouble(priceStr);
+            priceCounts.put(price, priceCounts.getOrDefault(price, 0) + 1);
+        }
         return priceCounts;
     }
 }
--- src/test/java/mainTests.java
+++ src/test/java/mainTests.java
@@ -8,12 +8,10 @@ import java.util.regex.Pattern;
 
 public class mainTests {
     Main main = new Main();
-
     /*
     Instead of : to separate key values, it could be -> (:, @, ^, *, %, !)
     Instead of separating pairs with , it is -> (##)
      */
-
     @Test
     public void patternMatcherTest(){
         String testCode = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016";
@@ -23,110 +21,50 @@ public class mainTests {
         Assert.assertTrue(matchFound);
     }
 
-    //##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##
-
-    @Test
-    public void makeKeyValueTest(){
-        String testCode = "naMe:Milk!price:3.23;type:Food^expiration:1/25/2016";
-        String tempString = main.formatStringData(testCode);
-        String expected = "naMe : Milk\n" +
-                "==========\n" +
-                "price : 3.23\n" +
-                "==========\n" +
-                "type : Food\n" +
-                "==========\n" +
-                "expiration : 1/25/2016";
-        Assert.assertEquals(expected, tempString);
-    }
-
-
-    @Test
-    public void separatePairsTest(){
-        String testCode = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##";
-        String tempString = main.separatePairs(testCode);
-        String expected = "NAME : MILK\n" +
-                "==========\n" +
-                "PRICE : 3.23\n" +
-                "==========\n" +
-                "TYPE : FOOD\n" +
-                "==========\n" +
-                "EXPIRATION : 1/25/2016\n" +
-                "-----------\n" +
-                "\n" +
-                "\n" +
-                "NAME : BREAD\n" +
-                "==========\n" +
-                "PRICE : 1.23\n" +
-                "==========\n" +
-                "TYPE : FOOD\n" +
-                "==========\n" +
-                "EXPIRATION : 1/02/2016\n" +
-                "-----------\n\n\n";
-        System.out.println(tempString);
-        Assert.assertEquals(expected, tempString);
-    }
-
-    @Test
-    public void addSecondColumn (){
-                String format = "%-20s %10S\n";
-
-                System.out.printf(format, "Name:", "Seen:");
-                System.out.println("====================");
-
-                System.out.printf(format, "Milk", "6 times");
-                System.out.printf(format, "Price: 3.23", "5 times");
-                System.out.println("--------------------");
-                System.out.printf(format, "Price: 1.23", "1 time");
-    }
-
-    //Instead of : to separate key values, it could be -> (:, @, ^, *, %, !)
-
     @Test
     public void matcherMethodsTest() {
-        String text = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##";
-       //String fullText = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
+        //REGEX
+        String fullText = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
         String patternString = "Name:(.*?)[;@*%!^]Price:(.*?)[;@*%!^]type:(.*?)[;@*%!^]expiration:(.*?)#";
         Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(text);
+        Matcher matcher = pattern.matcher(fullText);
 
+        //PRICES
+        Map<Double, Integer> appleMap = main.createApplesPriceMap(fullText);//0.25 and 0.23
+        Map<Double, Integer> cookieMap = main.createCookiePriceMap(fullText);//2.25
+        Map<Double, Integer> milkMap = main.createMilkPriceMap(fullText);//3.23 and 1.23
+        Map<Double, Integer> breadMap = main.createBreadPriceMap(fullText);//1.23
 
-        for (int i = 0; matcher.find(); i++) {
-            int seenCount = 0;
-//            String name = matcher.group(i);
-            System.out.println(new StringBuilder()
-                    .append(String.format("\nName: %-"+20+"s Seen: %d times", matcher.group(1), seenCount))
-                    .append(String.format("\n=================== %-"+6+"s ===================", ""))
-                    .append(String.format("\nPrice: %-"+19+"s Seen: %d times", matcher.group(2), seenCount))
-                    .append("\nType: " + matcher.group(3))
-                    .append("\nExpiration: " + matcher.group(4))
-                    .append(String.format("\n------------------- %-"+6+"s -------------------", ""))
-            );
-        }
-    }
-
-    @Test
-    public void findOccurences (){
-        String text = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##";
-        String fullText = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
-
-        //bread
-        //apples
-        //milk
-        //cookies
-        String breadRegex = "\\bb[a-zA-Z]*d\\b";    // Starts with 'b' and ends with 'd'
-        String applesRegex = "\\b[aA]pples\\b";    // Exact match for 'apples'
-        String milkRegex = "\\b[mM]ilk\\b";        // Exact match for 'milk'
-        String cookiesRegex = "\\b\\w*kies\\b";
-        //regex for each one first and last (example: for bread do a regex if it starts with b and ends with d)
-        //give a count of how many occurences of those 4 types
-        int breadCount = main.countOccurrences(text, breadRegex);
-        int applesCount = main.countOccurrences(text, applesRegex);
-        int milkCount = main.countOccurrences(text, milkRegex);
-        int cookiesCount = main.countOccurrences(fullText, cookiesRegex);
-        System.out.println("Bread count: " + breadCount);
-        System.out.println("Apples count: " + applesCount);
-        System.out.println("Milk count: " + milkCount);
-        System.out.println("Cookies count: " + cookiesCount);
+        StringBuilder sb = new StringBuilder();
+        //APPLES
+        sb.append(String.format("\nName: Apples%-"+18+"s Seen: %d times","",main.findApplesSeen(fullText)));
+        sb.append(String.format("\n=============== %-"+11+"s ================", ""));
+        sb.append(String.format("\nPrice: 0.25%-"+19+"s Seen: %d times","",appleMap.get(0.25)));
+        sb.append(String.format("\n---------------- %-"+10+"s ----------------", ""));
+        sb.append(String.format("\nPrice: 0.23%-"+19+"s Seen: %d times","",appleMap.get(0.23)));
+        sb.append("\n\n");
+        //COOKIES
+        sb.append(String.format("\nName: Cookies%-"+17+"s Seen: %d times","",main.findCookiesSeen(fullText)));
+        sb.append(String.format("\n=============== %-"+11+"s ================", ""));
+        sb.append(String.format("\nPrice: 2.25%-"+19+"s Seen: %d times","",cookieMap.get(2.25)));
+        sb.append(String.format("\n---------------- %-"+10+"s ----------------", ""));
+        sb.append("\n\n");
+        //MILK
+        sb.append(String.format("\nName: Milk%-"+20+"s Seen: %d times","",main.findMilkSeen(fullText)));
+        sb.append(String.format("\n=============== %-"+11+"s ================", ""));
+        sb.append(String.format("\nPrice: 0.25%-"+19+"s Seen: %d times","",milkMap.get(3.23)));
+        sb.append(String.format("\n---------------- %-"+10+"s ----------------", ""));
+        sb.append(String.format("\nPrice: 0.23%-"+19+"s Seen: %d times","",milkMap.get(1.23)));
+        sb.append("\n\n");
+        //BREAD
+        sb.append(String.format("\nName: Bread%-"+19+"s Seen: %d times","",main.findBreadSeen(fullText)));
+        sb.append(String.format("\n=============== %-"+11+"s ================", ""));
+        sb.append(String.format("\nPrice: 0.25%-"+19+"s Seen: %d times","",breadMap.get(1.23)));
+        sb.append(String.format("\n---------------- %-"+10+"s ----------------", ""));
+        sb.append("\n");
+        //ERRORS
+        sb.append(String.format("\nErrors:%-"+23+"s Seen: %d times","",4));
+        System.out.println(sb);
     }
 
     @Test
@@ -175,12 +113,29 @@ public class mainTests {
     public void createApplesPriceMapTest(){
         String fullText = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
         Map<Double, Integer> applesMap = new HashMap<>();
-        main.createApplesPriceMap(fullText);
-
-
+        applesMap = main.createApplesPriceMap(fullText);
+        int expected = 2;
+        int actual = applesMap.get(0.25);
+        Assert.assertEquals(expected,actual);
     }
 
+    @Test
+    public void createMilkPriceMapTest(){
+        String fullText = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
+        Map<Double, Integer> milkMap = new HashMap<>();
+        milkMap = main.createMilkPriceMap(fullText);
+        int expected = 5;
+        int actual = milkMap.get(3.23);
+        Assert.assertEquals(expected,actual);
+    }
 
-
-
+    @Test
+    public void createCookiesPriceMapTest(){
+        String fullText = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
+        Map<Double, Integer> cookieMap = new HashMap<>();
+        cookieMap = main.createCookiePriceMap(fullText);
+        int expected = 8;
+        int actual = cookieMap.get(2.25);
+        Assert.assertEquals(expected,actual);
+    }
 }
