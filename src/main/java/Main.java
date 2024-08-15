4989f1060f63be2aff0bc5ea8197962e10833ae4
Josue <josue@Zipcoders-MacBook-Pro-75.local>
2024-08-15 13:08:57 -0400

    Refactored. Complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
-
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,40 +9,56 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+
+
+    public static void main(String[] args) throws Exception{
+       try {
+           FileWriter writer = new FileWriter("src/main/resources/results.txt");
+           writer.write(printsResults());
+           writer.close();
+           System.out.println("Successfully wrote to the file.");
+       } catch (IOException e){
+           e.printStackTrace();
+       }
+    }
+
+
+
+    //Logic (my methods) down below ...
+
+
+
+
     public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
 
-    public static void main(String[] args) throws Exception{
+    private static String printsResults(){
         Main main = new Main();
-
-        //REGEX
+        //regex
         String fullText = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:2/25/2016##naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food%expiration:1/25/2016##naMe:CoOkieS;price:2.25;type:Food*expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;price:1.23;type:Food!expiration:4/25/2016##naMe:apPles;price:0.25;type:Food;expiration:1/23/2016##naMe:apPles;price:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food;expiration:1/04/2016##naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##naME:BreaD;price:1.23;type:Food@expiration:1/02/2016##NAMe:BrEAD;price:1.23;type:Food@expiration:2/25/2016##naMe:MiLK;priCe:;type:Food;expiration:1/11/2016##naMe:Cookies;price:2.25;type:Food;expiration:1/25/2016##naMe:Co0kieS;pRice:2.25;type:Food;expiration:1/25/2016##naMe:COokIes;price:2.25;type:Food;expiration:3/22/2016##naMe:COOkieS;Price:2.25;type:Food;expiration:1/25/2016##NAME:MilK;price:3.23;type:Food;expiration:1/17/2016##naMe:MilK;priCe:;type:Food;expiration:4/25/2016##naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##";
         String patternString = "Name:(.*?)[;@*%!^]Price:(.*?)[;@*%!^]type:(.*?)[;@*%!^]expiration:(.*?)#";
         Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(fullText);
-
-        //PRICES
+        //prices maps
         Map<Double, Integer> appleMap = main.createApplesPriceMap(fullText);//0.25 and 0.23
         Map<Double, Integer> cookieMap = main.createCookiePriceMap(fullText);//2.25
         Map<Double, Integer> milkMap = main.createMilkPriceMap(fullText);//3.23 and 1.23
         Map<Double, Integer> breadMap = main.createBreadPriceMap(fullText);//1.23
-
-        // STRING BUILDER
+        // complete stringbuilder
         StringBuilder sb = new StringBuilder();
         addCategory(sb, "Apples", main.findApplesSeen(fullText), appleMap, new double[]{0.25, 0.23});
         addCategory(sb, "Cookies", main.findCookiesSeen(fullText), cookieMap, new double[]{2.25});
         addCategory(sb, "Milk", main.findMilkSeen(fullText), milkMap, new double[]{3.23, 1.23});
         addCategory(sb, "Bread", main.findBreadSeen(fullText), breadMap, new double[]{1.23});
         sb.append(String.format("\nErrors:%-"+23+"s Seen: %d times", "", 4));
-        System.out.println(sb);
-
+        return sb.toString();
     }
 
     private static void addCategory(StringBuilder sb, String name, int seenCount, Map<Double, Integer> priceMap, double[] prices) {
-        sb.append(String.format("\nName: %7s%-"+18+"s Seen: %d times", name, "", seenCount));
+        sb.append(String.format("\nName: %7s%-"+17+"s Seen: %d times", name, "", seenCount));
         sb.append(String.format("\n=============== %-"+11+"s ================", ""));
         for (double price : prices) {
             sb.append(String.format("\nPrice: %.2f%-"+19+"s Seen: %d times", price, "", priceMap.getOrDefault(price, 0)));
